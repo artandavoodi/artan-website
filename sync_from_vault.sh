771aba2237429f356faa354d325affb92dc61bb9
Artan <artanda.sd@icloud.com>
2026-02-22 17:54:25 +0100

    Update site

--- sync_from_vault.sh
+++ sync_from_vault.sh
@@ -2,10 +2,12 @@
 set -euo pipefail
 
 VAULT="$HOME/Library/Mobile Documents/iCloud~md~obsidian/Documents/I/11_Publish"
-DEST="$HOME/Library/Mobile Documents/com~apple~CloudDocs/Documents/Site/artan-website/content_sync"
+DEST="$HOME/Documents/Site/artan-website/content_sync"
 
 mkdir -p "$DEST"
 rsync -av --delete \
   --exclude ".DO_NOT_EDIT" \
+  --exclude "DO_NOT_EDIT" \
+  --exclude "Do Not Edit" \
   --exclude "README.md" \
   "$VAULT"/ "$DEST"/
